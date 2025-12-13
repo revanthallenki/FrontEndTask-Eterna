edef3dbdf5ab773e5ca3f8754e8ab23b8a0ef4a5
Revanth Allenki <152966390+revanthallenki@users.noreply.github.com>
2025-12-13 11:08:15 +0530

    Update Portal.tsx

--- axiom-replica/src/components/UI/Portal.tsx
+++ axiom-replica/src/components/UI/Portal.tsx
@@ -4,13 +4,13 @@ import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
 export default function Portal({ children }: { children: React.ReactNode }) {
-  const [container, setContainer] = useState<HTMLElement | null>(null);
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    setContainer(document.body);
+    setMounted(true);
   }, []);
 
-  if (!container) return null;
+  if (!mounted) return null;
 
-  return createPortal(children, container);
+  return createPortal(children, document.body);
 }
